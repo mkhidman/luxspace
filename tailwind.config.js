7dca2539b1d0bb66ac835e18ff496511cd32b8f9
mkhidman <76956290+mkhidman@users.noreply.github.com>
2024-10-16 14:38:00 +0700

    update to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./**/*.html", "./**/*.js"],
-  },
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
   presets: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
       sm: "640px",
@@ -21,7 +21,7 @@ module.exports = {
 
       black: colors.black,
       white: colors.white,
-      gray: colors.coolGray,
+      gray: colors.gray,
       red: colors.red,
       yellow: colors.amber,
       green: colors.emerald,
